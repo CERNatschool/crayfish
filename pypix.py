7ebb8641d64bfb022f719e0afbf72f43049fb628
Richard Ladley <richard@langtonstarcentre>
2013-04-12 12:15:03 +0100

    Began frame and cluster classes

--- pypix.py
+++ pypix.py
@@ -1,6 +1,5 @@
 class PixelGrid(dict):
 
-
     def __missing__(self, key):
         if self.in_grid(key):
             return 0
@@ -37,12 +36,17 @@ class PixelGrid(dict):
         return float(self.max_count)/self.num_hits
 
     @property
-    def centre_of_mass(self):
-        weighted_hits = [tuple([self[hit] * coord for coord in hit])
-                for hit in self.hits]
-        x_coords, y_coords = zip(*weighted_hits)
-        total_weight = float(self.volume)
-        return (sum(x_coords)/total_weight, sum(y_coords)/total_weight)
+    def min_x(self):
+        return min([hit[0] for hit in self.hits])
+    @property
+    def max_x(self):
+        return max([hit[0] for hit in self.hits])
+    @property
+    def min_y(self):
+        return min([hit[1] for hit in self.hits])
+    @property
+    def max_y(self):
+        return max([hit[1] for hit in self.hits])
 
     def number_of_neighbours(self, pixel):
         x, y = pixel
@@ -68,3 +72,58 @@ class PixelGrid(dict):
         # max_neighbours = max(neighbours, key = lambda x: x[1])[1]
         # return max_neighbours, [pixel[0] for pixel in neighbours
         #         if pixel[1] == max_neighbours]
+
+class Frame(PixelGrid):
+
+    def calculate_clusters(self):
+        self.clusters = []
+
+    @staticmethod
+    def from_file(filepath, file_format = "lsc"):
+        frame = Frame()
+        if file_format == "lsc":
+            with open(filepath) as f:
+                for line in f:
+                    if line[:2] == "//":
+                        continue
+                    pixel, count = line.strip().split()
+                    pixel = tuple([int(coord) for coord in pixel.split(",")])
+                    frame[pixel] = count
+            frame.width = 256
+            frame.height = 256
+        else:
+            raise Exception("File format not supported: " + file_format)
+        return frame
+
+class Cluster(PixelGrid):
+
+    @property
+    def width(self):
+        return self.max_x
+
+    @property
+    def width(self):
+        return self.max_y
+
+    @property
+    def geometric_centre(self):
+        return (self.width/2.0, self/height/2.0)
+
+    @property
+    def centre_of_mass(self):
+        weighted_hits = [tuple([self[hit] * coord for coord in hit])
+                for hit in self.hits]
+        x_coords, y_coords = zip(*weighted_hits)
+        total_weight = float(self.volume)
+        return (sum(x_coords)/total_weight, sum(y_coords)/total_weight)
+
+    @property
+    def radius(self):
+        # Call centre of mass once to save computing multiple times
+        cofm_x, cofm_y = self.centre_of_mass
+        distances_squared = []
+        for hit in self.hits:
+            x_diff = hit[0] - cofm_x
+            y_diff = hit[1] - cofm_x
+            distances_squared.append(x_diff**2 + y_diff**2)
+        return max(distances_squared)**0.5
