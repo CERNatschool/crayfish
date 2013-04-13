a6fed15dfe034de5a4f78546e417f2b54e04320a
Richard Ladley <richard@langtonstarcentre>
2013-04-13 17:02:48 +0100

    Reordered UI drawing code

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ class MainWindow(wx.Frame):
         file_menu = wx.Menu()
 
         menu_quit = file_menu.Append(wx.ID_EXIT)
-        self.Bind(wx.EVT_MENU, self.on_quit, menu_quit)
         menu_open = file_menu.Append(wx.ID_OPEN)
+
+        self.Bind(wx.EVT_MENU, self.on_quit, menu_quit)
         self.Bind(wx.EVT_MENU, self.on_open, menu_open)
 
         menu_bar.Append(file_menu, "&File")
@@ -36,35 +37,37 @@ class MainWindow(wx.Frame):
     def init_window(self):
         window_panel = wx.Panel(self)
         window_panel.SetBackgroundColour("#5f6059")
+
         h_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        file_sizer = wx.BoxSizer(wx.VERTICAL)
-
-        self.file_tree = FileTreeCtrl(window_panel, size=(200, -1))
-        file_sizer.Add(self.file_tree, 1, wx.EXPAND | wx.TOP | wx.RIGHT | wx.LEFT)
-
-        ext_input_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        # File Browser Column
         ext_label = wx.StaticText(window_panel, label="Extension:")
         self.ext_field = wx.ComboBox(window_panel, value="*.lsc",choices=["*.lsc", "*.ascii"])
+        self.file_tree = FileTreeCtrl(window_panel, size=(200, -1))
+        open_button = wx.Button(window_panel, wx.ID_OPEN, label="Open Folder")
+
+        self.Bind(wx.EVT_BUTTON, self.on_open, open_button)
+
+        file_sizer = wx.BoxSizer(wx.VERTICAL)
+        file_sizer.Add(self.file_tree, 1, wx.EXPAND | wx.TOP | wx.RIGHT | wx.LEFT)
+        ext_input_sizer = wx.BoxSizer(wx.HORIZONTAL)
         ext_input_sizer.Add(ext_label, 0, wx.TOP, 5)
         ext_input_sizer.Add(self.ext_field, 0,)
         file_sizer.Add(ext_input_sizer, 0, wx.ALIGN_RIGHT)
-
-        open_button = wx.Button(window_panel, wx.ID_OPEN, label="Open Folder")
-        self.Bind(wx.EVT_BUTTON, self.on_open, open_button)
         file_sizer.Add(open_button, 0, wx.ALIGN_RIGHT)
         h_sizer.Add(file_sizer, 0, wx.EXPAND | wx.ALL, 5)
-
-        centre_sizer = wx.BoxSizer(wx.VERTICAL)
+        # Trace/Graph View Column
         display_notebook = wx.Notebook(window_panel, style = wx.NB_NOPAGETHEME)
         display_trace = TraceRender(display_notebook)
         self.file_tree.renderer = display_trace
         display_notebook.AddPage(display_trace, "Trace")
         display_graph = wx.Panel(display_notebook)
         display_notebook.AddPage(display_graph, "Graph")
+
+        centre_sizer = wx.BoxSizer(wx.VERTICAL)
         centre_sizer.Add(display_notebook, 1, wx.EXPAND)
         h_sizer.Add(centre_sizer, 1, wx.EXPAND)
 
-        settings_sizer = wx.BoxSizer(wx.VERTICAL)
+        # Settings Column
         settings_notebook = wx.Notebook(window_panel, size = (300, -1), style=wx.NB_NOPAGETHEME)
         settings_view = wx.Panel(settings_notebook)
         settings_notebook.AddPage(settings_view, "View")
@@ -72,6 +75,8 @@ class MainWindow(wx.Frame):
         settings_notebook.AddPage(settings_plot, "Plot")
         settings_classify = wx.Panel(settings_notebook)
         settings_notebook.AddPage(settings_classify, "Classify")
+
+        settings_sizer = wx.BoxSizer(wx.VERTICAL)
         settings_sizer.Add(settings_notebook, 1, wx.EXPAND)
         h_sizer.Add(settings_sizer, 0, wx.EXPAND)
 
